ec4419d16c4ba6787034dc3a2d2377650a340855
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-07-28 23:57:47 +0530

    Worked on first server

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app=express();
 
+app.get("/",function(req, res){
+    console.log(req);
+    res.send("Hello, World!");
+});
+
 app.listen(3000,function () {
     console.log("Server Started at port 3000");
 });
