60e1ecfbc83839de0e93eb24297beff71440eaba
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-07-29 00:58:23 +0530

    Continued with Intro to ExpressJS

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -7,6 +7,14 @@ app.get("/",function(req, res){
     res.send("Hello, World!");
 });
 
+app.get("/contact",function(req, res){
+    res.send("Contact me at: contactharshitsaraswat@gmail.com");
+});
+
+app.get("/about",function(req, res){
+    res.send("Developed By: Harshit Saraswat (harshit-saraswat)");
+});
+
 app.listen(3000,function () {
     console.log("Server Started at port 3000");
 });
