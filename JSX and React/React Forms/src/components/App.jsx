b06441efccc451530b7141065a543db7ef90f89c
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-09-25 23:57:09 +0530

    Started with React Forms

---
+++ JSX and React/React Forms/src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function App() {
+  const [isMouseOver, setMouseOver] = React.useState(false);
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
