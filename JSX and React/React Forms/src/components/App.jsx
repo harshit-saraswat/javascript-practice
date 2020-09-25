ab9ddc6f41563ed11b41e3e93e939925bc110ada
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-09-26 00:12:30 +0530

    Completed React Forms Challenges

--- JSX and React/React Forms/src/components/App.jsx
+++ JSX and React/React Forms/src/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 function App() {
   const [isMouseOver, setMouseOver] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [heading, setHeading] = React.useState("Hello!");
   function handleMouseOver() {
     setMouseOver(true);
   }
@@ -10,17 +12,34 @@ function App() {
     setMouseOver(false);
   }
 
+  function handleInputChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleOnClick(event) {
+    setHeading("Hello, " + name + "!");
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button
-        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        Submit
-      </button>
+      <h1>{heading}</h1>
+      <form onSubmit={handleOnClick}>
+        <input
+          onChange={handleInputChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
