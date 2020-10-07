9cb10ec6c86fee9c4621d27ae6072201b9e63dac
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-10-07 23:02:30 +0530

    Implemented Complex React States with ES6 Spread Operator

---
+++ JSX and React/Complex React States 2 with ES6 Spread Operator/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="email" placeholder="Email" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
