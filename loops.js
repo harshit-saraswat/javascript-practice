dd9c1e61c11ba76161a708836ea82296dfe32eaa
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-10-10 20:08:21 +0530

    Added For Loop

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
-// ForEach Loop
-
 data=['a','b','c','d'];
+
+// For Loop
+for (var i = 0; i < data.length; i++) {
+  console.log(data[i]);
+}
+
+// ForEach Loop
 data.forEach(val=>{
 	console.log(val);
 });
