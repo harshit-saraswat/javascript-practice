5c60c70bb0d49021548f1e830750e8f9026a9828
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-10-17 21:07:26 +0530

    Added If-Else

--- conditions.js
+++ conditions.js
@@ -3,3 +3,10 @@ var time = new Date().getHours();
 if (time < 20) {
   console.log("Good Day!");
 }
+
+// If-Else
+if (time < 20) {
+  console.log("Good Day!");
+}else{
+	console.log("Good Night!");
+}
