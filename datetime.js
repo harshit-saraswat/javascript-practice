2e798d8c916dd940480f88cd4f909ee5501f48ac
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-10-31 22:45:56 +0530

    Added date time

---
+++ datetime.js
@@ -0,0 +1,5 @@
+// Initialize as current date object
+var d = new Date();
+
+// Initialize as custom date object
+var d = new Date(2018, 11, 24, 10, 33, 30, 0); // Arguments- Date(year, month, day, hours, minutes, seconds, milliseconds)
