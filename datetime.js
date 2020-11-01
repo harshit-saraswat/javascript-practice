7c105692339ede0e05e62b1dec26831619088b9d
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-11-01 23:22:11 +0530

    Date initialization contd

--- datetime.js
+++ datetime.js
@@ -3,3 +3,8 @@ var d = new Date();
 
 // Initialize as custom date object
 var d = new Date(2018, 11, 24, 10, 33, 30, 0); // Arguments- Date(year, month, day, hours, minutes, seconds, milliseconds)
+
+// Initializing date object with date string
+var d = new Date("October 13, 2014 11:13:00");
+
+// By default date format:-Sun Nov 01 2020 23:21:21 GMT+0530 (India Standard Time)
