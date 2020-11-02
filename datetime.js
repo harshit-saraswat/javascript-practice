75e0c415bb390057e9046b45654c8566c612ef2c
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-11-02 22:50:18 +0530

    Added Date formats and conversions

--- datetime.js
+++ datetime.js
@@ -8,3 +8,12 @@ var d = new Date(2018, 11, 24, 10, 33, 30, 0); // Arguments- Date(year, month, d
 var d = new Date("October 13, 2014 11:13:00");
 
 // By default date format:-Sun Nov 01 2020 23:21:21 GMT+0530 (India Standard Time)
+
+// Date to UTC Date
+utcDate=d.toUTCString();
+
+// Date to Date String
+ds=d.toDateString();
+
+// Date String - ISO Format
+isoDate=d.toISOString();
