cb070eff3e094796270d6d3bf9c8df7c37c11b20
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-11-03 23:04:42 +0530

    Added Date formats and get methods

--- datetime.js
+++ datetime.js
@@ -17,3 +17,36 @@ ds=d.toDateString();
 
 // Date String - ISO Format
 isoDate=d.toISOString();
+
+// Parse Date String to milliseconds
+var msec = Date.parse("March 21, 2012");
+
+// Get time from date
+var d=new Date();
+var time=d.getTime();
+
+// Get Full Year
+var year=d.getFullYear();
+
+// Get Month
+var month=d.getMonth();
+
+// Get Date
+var date=d.getDate();
+
+// Get Hours
+var hours=d.getHours();
+
+// Get Minutes
+var mins=d.getMinutes();
+
+// Get Seconds
+var secs=d.getSeconds();
+
+// Get Milliseconds
+var msecs=d.getMilliseconds();
+
+// Get Day
+var day=d.getDay();
+
+
